f266c63cc53ce741f2dc3689de9728a91ab483bc
sunray23 <141627341+sunray23@users.noreply.github.com>
2026-05-20 15:35:09 +0100

    Update streamlit_app.py
    json()

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ ingredients_list = st.multiselect(
 )
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json)
+st.text(smoothiefroot_response.json())
 if ingredients_list:
     st.text(ingredients_list)
 
