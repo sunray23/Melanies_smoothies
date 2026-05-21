9d820d76221cce32c2c510cfcfefabacdd373e65
sunray23 <141627341+sunray23@users.noreply.github.com>
2026-05-21 09:47:26 +0100

    Update streamlit_app.py
    iloc

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,9 +61,12 @@ except Exception as e:
 # Query the fruit options
 try:
     my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-    st.dataframe(data=my_dataframe, use_container_width=True)
-    st.stop()
-
+    #st.dataframe(data=my_dataframe, use_container_width=True)
+    #st.stop()
+    # convert the snowpark dataframe to pandas
+    pd_df=my+dataframe.to_pandas()
+    #st.dataframe(pd_df)
+    #st.stop
 except Exception as e:
     st.error(f"Error querying fruit options: {e}")
     st.stop()
@@ -81,6 +84,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
         #st.text(smoothiefroot_response.json())
